c2c2fb896993d6262d2dc25cd099bedfba95e8e5
Piotr Balas <balas.piotr@gmail.com>
2020-03-01 22:54:57 +0100

    Add Domain Test Project

---
+++ Billing.Domain.Tests/InvoiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Billing.Domain.Tests
+{
+    public class InvoiceTests
+    {
+        [Fact]
+        public void Create_EmitInvoiceCreatedEvent()
+        {
+
+        }
+    }
+}
